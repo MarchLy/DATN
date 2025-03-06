f8d11aef9e3d7939f7376ffdd2c926219cb79a21
Ph54005 <zennodeptrai@gmail.com>
2025-03-06 21:45:59 +0700

    update

---
+++ admin-fe-template-spring-boot-thymeleaf-master/src/main/java/com/farhan/staradmin/controller/GiamGiaController/DotGiamGiaController/AddDotGiamGiaController.java
@@ -0,0 +1,12 @@
+package com.farhan.staradmin.controller.GiamGiaController.DotGiamGiaController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class AddDotGiamGiaController {
+    @GetMapping("/ADDdotGiamGia")
+    public String show(){
+
+        return "pages/QuanLyGiamGia/DotGiamGiaView/AddDotGiamGia";
+    }
+}

b63fc891e8f8b24aa922e39fc389b8bbfa155a66
Dung <dungdamvan2005@gmail.com>
2025-03-11 18:00:21 +0700

    dung moi update

--- admin-fe-template-spring-boot-thymeleaf-master/src/main/java/com/farhan/staradmin/controller/SanPhamController/KichThuocController.java
+++ admin-fe-template-spring-boot-thymeleaf-master/src/main/java/com/farhan/staradmin/controller/SanPhamController/KichThuocController.java
@@ -1,4 +1,10 @@
 package com.farhan.staradmin.controller.SanPhamController;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/kich-thuoc")
 public class KichThuocController {
+
 }
